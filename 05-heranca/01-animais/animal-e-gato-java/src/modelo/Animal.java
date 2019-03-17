972f06ce927103b2eca4c44d93d610645b38b589
Hylson <hvescovi@gmail.com>
2019-03-17 07:15:36 -0300

    incluindo código sobre herança

---
+++ 05-heranca/01-animais/animal-e-gato-java/src/modelo/Animal.java
@@ -0,0 +1,13 @@
+package modelo;
+
+public class Animal {
+
+    public Animal() {
+        System.out.println("Animal criado");
+    }
+    public void comer() {
+        System.out.println("Animal comendo");
+    }
+}
+
+
---
+++ 05-heranca/01-animais/animal-e-gato-java/src/modelo/Gato.java
@@ -0,0 +1,8 @@
+package modelo;
+
+public class Gato extends Animal {
+    
+    public Gato(){
+        System.out.println("Gato criado");
+    }
+}
