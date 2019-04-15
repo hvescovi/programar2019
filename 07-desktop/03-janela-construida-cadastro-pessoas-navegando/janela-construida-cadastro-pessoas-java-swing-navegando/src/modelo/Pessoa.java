3014af5f1c58c53a8dfb41002b557a99b755c6cb
Hylson <hvescovi@gmail.com>
2019-04-15 14:20:21 -0300

    cadastro de pessoas navegando

---
+++ 07-desktop/03-janela-construida-cadastro-pessoas-navegando/janela-construida-cadastro-pessoas-java-swing-navegando/src/modelo/Pessoa.java
@@ -0,0 +1,16 @@
+package modelo;
+
+public class Pessoa {
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getEndereco() { return endereco; }
+    public void setEndereco(String e) {endereco = e; }
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String t) {telefone = t; }
+    public Pessoa(String n, String e, String t) {
+        nome = n; endereco = e; telefone = t;   }
+}
