59a5432a08aeba7e65a4d73b527c56d6c928e446
Hylson <hvescovi@gmail.com>
2019-10-07 13:32:15 -0300

    ajustes nos exemplos jquery e integração

--- 09-integracao/05-pessoas-front-e-back-com-javascript/05-pessoas-front-e-back-com-javascript-python/modelo.py
+++ 09-integracao/05-pessoas-front-e-back-com-javascript/05-pessoas-front-e-back-com-javascript-python/modelo.py
@@ -1,4 +1,5 @@
 from peewee import *
+import os
 
 arq = './pessoas-front-end.db'
 db = SqliteDatabase(arq)
@@ -13,8 +14,14 @@ class Pessoa(BaseModel):
     telefone = CharField()
     
 if __name__ == "__main__":
+    # apagar o arquivo caso ele exista
+    if os.path.exists(arq):
+        os.remove(arq)
+
     db.connect()
     db.create_tables([Pessoa])
     joao = Pessoa.create(nome="Joao da Silva", 
         endereco="Casa 9", telefone="3541-1230")
+    maria = Pessoa.create(nome = "Maria de Oliveira", 
+        endereco = "Beco das Flores, S/N", telefone="não possui")
     print(joao.nome, ",", joao.endereco, ",", joao.telefone)
