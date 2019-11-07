615bf8c0c71878d206a44322b2d6505100b3c882
Hylson <hvescovi@gmail.com>
2019-11-06 21:04:03 -0300

    exercicios sobre permissoes

---
+++ 11-sql/exercicios-permissoes/01.sql
@@ -0,0 +1,65 @@
+USE Loja;
+
+CREATE TABLE cliente(
+	ID_CLIENTE INT NOT NULL,
+	NOM_CLIENTE VARCHAR(60) NOT NULL,
+	CPF_CLIENTE VARCHAR(15)  NOT NULL,
+	PRIMARY KEY (ID_CLIENTE)
+	);
+
+CREATE TABLE produto(
+	ID_PRODUTO INT NOT NULL,
+	NOME_PRODUTO VARCHAR(60) NOT NULL,
+	PRECO_PRODUTO INT NOT NULL,
+	PRIMARY KEY (ID_PRODUTO)
+	);
+
+
+CREATE TABLE pedido(
+ 	ID_PEDIDO INT NOT NULL,
+ 	ID_PRODUTO INT NOT NULL,
+ 	ID_CLIENTE INT NOT NULL,
+ 	PRIMARY KEY (ID_PEDIDO),
+ 	FOREIGN KEY (ID_PRODUTO) REFERENCES produto (ID_PRODUTO),
+ 	FOREIGN KEY (ID_CLIENTE) REFERENCES cliente (ID_CLIENTE)
+ 	);
+ 	
+	CREATE USER gerente IDENTIFIED BY 'password';
+	CREATE USER cliente IDENTIFIED BY 'password';
+	CREATE USER vendedor IDENTIFIED BY 'password';
+
+	
+
+\\PERMISSÕES DO VENDEDOR
+	GRANT SELECT ON Loja.cliente TO vendedor;
+	GRANT SELECT ON Loja.pedido TO vendedor;
+	GRANT SELECT ON Loja.produto TO vendedor;
+
+	GRANT INSERT ON Loja.cliente TO vendedor;
+	GRANT INSERT ON Loja.pedido TO vendedor;
+	
+
+	GRANT UPDATE ON Loja.cliente TO vendedor;
+	GRANT UPDATE ON Loja.pedido TO vendedor;
+ 	
+
+\\PERMISSÕES DO GERENTE
+	GRANT DELETE ON Loja.cliente TO gerente;
+	GRANT DELETE ON Loja.pedido TO gerente;
+	GRANT DELETE ON Loja.produto TO gerente;
+
+	GRANT INSERT ON Loja.cliente TO gerente;
+	GRANT INSERT ON Loja.pedido TO gerente;
+	GRANT INSERT ON Loja.produto TO gerente;
+
+	GRANT UPDATE ON Loja.cliente TO gerente;
+	GRANT UPDATE ON Loja.pedido TO gerente;
+ 	GRANT UPDATE ON Loja.produto TO gerente;
+ 
+ 
+ //PERMISSÕES CLIENTE
+ 
+  	GRANT SELECT ON Loja.pedido TO cliente WHERE cliente.ID_CLIENTE = 1; 
+ 
+ 
+ 
